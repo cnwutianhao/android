c24d16229e7e18e0e1dc58ecc39a72dcc12699b7
wutianhao <cnwutianhao@gmail.com>
2023-02-28 16:53:27 +0800

    沉浸式状态栏

--- lol/app/src/main/java/com/tyhoo/android/lol/ui/MainActivity.kt
+++ lol/app/src/main/java/com/tyhoo/android/lol/ui/MainActivity.kt
@@ -4,16 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.tyhoo.android.lol.R
 import com.tyhoo.android.lol.domain.BottomNavItem
 import dagger.hilt.android.AndroidEntryPoint
@@ -32,6 +36,17 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun LolApp(heroesViewModel: HeroesViewModel, itemsViewModel: ItemsViewModel) {
+    val systemUiController = rememberSystemUiController()
+    val useDarkIcons = MaterialTheme.colors.isLight
+
+    // 设置状态栏颜色
+    SideEffect {
+        systemUiController.setStatusBarColor(
+            color = Color.Transparent,
+            darkIcons = useDarkIcons
+        )
+    }
+
     val navController = rememberNavController()
     val items = listOf(
         BottomNavItem("heroes", "Heroes", R.drawable.lol_heroes),
@@ -40,39 +55,42 @@ fun LolApp(heroesViewModel: HeroesViewModel, itemsViewModel: ItemsViewModel) {
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     val currentRoute = navBackStackEntry?.destination?.route ?: "heroes"
 
-    Scaffold(
-        bottomBar = {
-            BottomNavigation {
-                items.forEach { item ->
-                    BottomNavigationItem(
-                        icon = {
-                            Icon(
-                                painter = painterResource(id = item.drawableId),
-                                contentDescription = item.label
-                            )
-                        },
-                        label = { Text(item.label) },
-                        selected = currentRoute == item.route,
-                        onClick = {
-                            navController.navigate(item.route) {
-                                launchSingleTop = true
+    // 在Scaffold外层添加一个背景色，以免系统状态栏被Scaffold的背景色遮挡
+    Box(modifier = Modifier.background(MaterialTheme.colors.background)) {
+        Scaffold(
+            bottomBar = {
+                BottomNavigation {
+                    items.forEach { item ->
+                        BottomNavigationItem(
+                            icon = {
+                                Icon(
+                                    painter = painterResource(id = item.drawableId),
+                                    contentDescription = item.label
+                                )
+                            },
+                            label = { Text(item.label) },
+                            selected = currentRoute == item.route,
+                            onClick = {
+                                navController.navigate(item.route) {
+                                    launchSingleTop = true
+                                }
                             }
-                        }
-                    )
+                        )
+                    }
                 }
             }
-        }
-    ) { innerPadding ->
-        Box(Modifier.padding(innerPadding)) {
-            NavHost(
-                navController = navController,
-                startDestination = "heroes"
-            ) {
-                composable("heroes") {
-                    HeroesScreen(viewModel = heroesViewModel)
-                }
-                composable("items") {
-                    ItemsScreen(viewModel = itemsViewModel)
+        ) { innerPadding ->
+            Box(Modifier.padding(innerPadding)) {
+                NavHost(
+                    navController = navController,
+                    startDestination = "heroes"
+                ) {
+                    composable("heroes") {
+                        HeroesScreen(viewModel = heroesViewModel)
+                    }
+                    composable("items") {
+                        ItemsScreen(viewModel = itemsViewModel)
+                    }
                 }
             }
         }
