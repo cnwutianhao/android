be195063fed7675647e8d607efcbe825adfb8382
wutianhao <cnwutianhao@gmail.com>
2024-01-15 16:44:42 +0800

    设置沉浸式状态栏、导航栏

--- mvvm/app/src/main/java/com/tyhoo/android/mvvm/MainActivity.kt
+++ mvvm/app/src/main/java/com/tyhoo/android/mvvm/MainActivity.kt
@@ -2,11 +2,15 @@ package com.tyhoo.android.mvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tyhoo.android.mvvm.util.LayoutUtils
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 沉浸式状态栏、导航栏
+        LayoutUtils.setImmersive(window, findViewById(R.id.root_layout))
     }
 }
---
+++ mvvm/app/src/main/java/com/tyhoo/android/mvvm/util/LayoutUtils.kt
@@ -0,0 +1,38 @@
+package com.tyhoo.android.mvvm.util
+
+import android.graphics.Color
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+object LayoutUtils {
+
+    /**
+     * 沉浸式状态栏、导航栏
+     */
+    fun setImmersive(window: Window, view: View) {
+        val uiOptions = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+                or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR)
+
+        window.decorView.systemUiVisibility = uiOptions
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = Color.TRANSPARENT
+        window.navigationBarColor = Color.TRANSPARENT
+
+        // 防止状态栏、导航栏和布局重叠
+        ViewCompat.setOnApplyWindowInsetsListener(view) { v, insets ->
+            val statusBarWindow =
+                insets.getInsets(WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout())
+            val navigationBarWindow =
+                insets.getInsets(WindowInsetsCompat.Type.navigationBars() or WindowInsetsCompat.Type.displayCutout())
+            v.setPadding(0, statusBarWindow.top, 0, navigationBarWindow.bottom)
+            insets
+        }
+    }
+}
