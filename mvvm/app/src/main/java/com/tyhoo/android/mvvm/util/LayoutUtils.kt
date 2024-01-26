d27bb64ae0d2eefe7886bd136bc73105d5ecdf9a
wutianhao <cnwutianhao@gmail.com>
2024-01-26 18:09:50 +0800

    修改沉浸式状态栏、导航栏

--- mvvm/app/src/main/java/com/tyhoo/android/mvvm/util/LayoutUtils.kt
+++ mvvm/app/src/main/java/com/tyhoo/android/mvvm/util/LayoutUtils.kt
@@ -1,10 +1,11 @@
 package com.tyhoo.android.mvvm.util
 
 import android.graphics.Color
+import android.os.Build
 import android.view.View
 import android.view.Window
-import android.view.WindowManager
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 
 object LayoutUtils {
@@ -13,26 +14,24 @@ object LayoutUtils {
      * 沉浸式状态栏、导航栏
      */
     fun setImmersive(window: Window, view: View) {
-        val uiOptions = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-                or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
 
-        window.decorView.systemUiVisibility = uiOptions
-        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
         window.statusBarColor = Color.TRANSPARENT
         window.navigationBarColor = Color.TRANSPARENT
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            window.navigationBarDividerColor = Color.TRANSPARENT
+        }
 
-        // 防止状态栏、导航栏和布局重叠
         ViewCompat.setOnApplyWindowInsetsListener(view) { v, insets ->
-            val statusBarWindow =
-                insets.getInsets(WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout())
-            val navigationBarWindow =
-                insets.getInsets(WindowInsetsCompat.Type.navigationBars() or WindowInsetsCompat.Type.displayCutout())
+            val statusBarWindow = insets
+                .getInsets(WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout())
             v.setPadding(0, statusBarWindow.top, 0, 0)
             insets
+        }
+
+        WindowCompat.getInsetsController(window, view).apply {
+            isAppearanceLightStatusBars = true
+            isAppearanceLightNavigationBars = true
         }
     }
 }
