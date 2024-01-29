8f851de04aeacc4a831ad8076b602129dbb05a48
wutianhao <cnwutianhao@gmail.com>
2024-01-29 17:27:26 +0800

    只在当前不在目标页面时才会导航到相应的目标页面

--- mvvm/app/src/main/java/com/tyhoo/android/mvvm/ui/MainFragment.kt
+++ mvvm/app/src/main/java/com/tyhoo/android/mvvm/ui/MainFragment.kt
@@ -1,14 +1,12 @@
 package com.tyhoo.android.mvvm.ui
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
 import com.tyhoo.android.mvvm.R
-import com.tyhoo.android.mvvm.base.TAG
 import com.tyhoo.android.mvvm.databinding.FragmentMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -34,19 +32,18 @@ class MainFragment : Fragment() {
         val navController = navHostFragment.navController
 
         binding.mainBottomNav.setOnItemSelectedListener { item ->
+            val currentDestinationId = navController.currentDestination?.id
+
             when (item.itemId) {
-                R.id.menu_hero -> {
-                    Log.d(TAG, "英雄")
+                R.id.menu_hero -> if (currentDestinationId != R.id.hero_list_fragment) {
                     navController.navigate(R.id.action_to_hero_list)
                 }
 
-                R.id.menu_item -> {
-                    Log.d(TAG, "装备")
+                R.id.menu_item -> if (currentDestinationId != R.id.item_list_fragment) {
                     navController.navigate(R.id.action_to_item_list)
                 }
 
-                R.id.menu_rune -> {
-                    Log.d(TAG, "铭文")
+                R.id.menu_rune -> if (currentDestinationId != R.id.rune_list_fragment) {
                     navController.navigate(R.id.action_to_rune_list)
                 }
 
