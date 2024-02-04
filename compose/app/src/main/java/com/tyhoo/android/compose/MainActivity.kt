6bd24de5a47225918ddbe1d74ecc6d4a5ab4e160
wutianhao <cnwutianhao@gmail.com>
2024-02-04 10:33:06 +0800

    沉浸式状态栏修改

--- compose/app/src/main/java/com/tyhoo/android/compose/MainActivity.kt
+++ compose/app/src/main/java/com/tyhoo/android/compose/MainActivity.kt
@@ -9,10 +9,15 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowCompat
+import com.google.accompanist.insets.ProvideWindowInsets
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.tyhoo.android.compose.ui.HomeScreen
 import com.tyhoo.android.compose.ui.theme.ComposeTheme
 import com.tyhoo.android.compose.util.ScreenUtils
@@ -24,19 +29,22 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        enableEdgeToEdge()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             ComposeTheme {
-                Box(modifier = Modifier.fillMaxSize()) {
-                    Image(
-                        painter = painterResource(id = R.drawable.bg_main),
-                        contentDescription = null,
-                        contentScale = ContentScale.FillBounds,
-                        modifier = Modifier.fillMaxSize()
+                ProvideWindowInsets {
+                    rememberSystemUiController().setStatusBarColor(
+                        Color.Transparent, darkIcons = MaterialTheme.colors.isLight
                     )
 
-                    val statusBarHeight = ScreenUtils.getStatusBarHeight(this@MainActivity)
-                    Column(modifier = Modifier.padding(top = (statusBarHeight - 32).dp)) {
+                    Box(modifier = Modifier.fillMaxSize()) {
+                        Image(
+                            painter = painterResource(id = R.drawable.bg_main),
+                            contentDescription = null,
+                            contentScale = ContentScale.FillBounds,
+                            modifier = Modifier.fillMaxSize()
+                        )
+
                         HomeScreen()
                     }
                 }
