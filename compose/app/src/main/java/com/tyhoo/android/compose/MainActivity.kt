4b351defc229cb8c9a84e159f48fb86be5187cb1
wutianhao <cnwutianhao@gmail.com>
2024-02-04 10:38:54 +0800

    删除无效代码

--- compose/app/src/main/java/com/tyhoo/android/compose/MainActivity.kt
+++ compose/app/src/main/java/com/tyhoo/android/compose/MainActivity.kt
@@ -3,24 +3,19 @@ package com.tyhoo.android.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
 import com.google.accompanist.insets.ProvideWindowInsets
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.tyhoo.android.compose.ui.HomeScreen
 import com.tyhoo.android.compose.ui.theme.ComposeTheme
-import com.tyhoo.android.compose.util.ScreenUtils
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
