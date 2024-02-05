d50379076e066baeff45b46a594f17992700272f
wutianhao <cnwutianhao@gmail.com>
2024-02-05 09:26:55 +0800

    使用 enableEdgeToEdge 实现沉浸式

--- compose/app/src/main/java/com/tyhoo/android/compose/MainActivity.kt
+++ compose/app/src/main/java/com/tyhoo/android/compose/MainActivity.kt
@@ -3,17 +3,14 @@ package com.tyhoo.android.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.core.view.WindowCompat
-import com.google.accompanist.insets.ProvideWindowInsets
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import androidx.core.view.ViewCompat
 import com.tyhoo.android.compose.ui.HomeScreen
 import com.tyhoo.android.compose.ui.theme.ComposeTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -24,24 +21,18 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        WindowCompat.setDecorFitsSystemWindows(window, false)
+        enableEdgeToEdge()
         setContent {
             ComposeTheme {
-                ProvideWindowInsets {
-                    rememberSystemUiController().setStatusBarColor(
-                        Color.Transparent, darkIcons = MaterialTheme.colors.isLight
+                Box(modifier = Modifier.fillMaxSize()) {
+                    Image(
+                        painter = painterResource(id = R.drawable.bg_main),
+                        contentDescription = null,
+                        contentScale = ContentScale.FillBounds,
+                        modifier = Modifier.fillMaxSize()
                     )
 
-                    Box(modifier = Modifier.fillMaxSize()) {
-                        Image(
-                            painter = painterResource(id = R.drawable.bg_main),
-                            contentDescription = null,
-                            contentScale = ContentScale.FillBounds,
-                            modifier = Modifier.fillMaxSize()
-                        )
-
-                        HomeScreen()
-                    }
+                    HomeScreen()
                 }
             }
         }
