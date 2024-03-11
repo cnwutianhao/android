3b23fc844731836d04a41baa4f49021c80e04765
cnwutianhao <cnwutianhao@gmail.com>
2024-03-11 17:10:40 +0800

    修改沉浸式状态栏、导航栏

--- mvvm/app/src/main/java/com/tyhoo/android/mvvm/ui/MainActivity.kt
+++ mvvm/app/src/main/java/com/tyhoo/android/mvvm/ui/MainActivity.kt
@@ -2,8 +2,10 @@ package com.tyhoo.android.mvvm.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.tyhoo.android.mvvm.R
-import com.tyhoo.android.mvvm.util.LayoutUtils
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -11,9 +13,12 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
-        // 沉浸式状态栏、导航栏
-        LayoutUtils.setImmersive(window, findViewById(R.id.root_layout))
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.root_layout)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
+            insets
+        }
     }
 }
