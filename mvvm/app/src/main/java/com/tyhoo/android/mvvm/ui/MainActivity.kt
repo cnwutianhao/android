f47fd0873305541110b5e673533847094524a22b
cnwutianhao <cnwutianhao@gmail.com>
2024-04-01 15:25:27 +0800

    完善列表页内容

--- mvvm/app/src/main/java/com/tyhoo/android/mvvm/ui/MainActivity.kt
+++ mvvm/app/src/main/java/com/tyhoo/android/mvvm/ui/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.viewModels
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.tyhoo.android.mvvm.R
+import com.tyhoo.android.mvvm.databinding.ActivityMainBinding
 import com.tyhoo.android.mvvm.viewmodel.ArcanaListViewModel
 import com.tyhoo.android.mvvm.viewmodel.EquipmentListViewModel
 import com.tyhoo.android.mvvm.viewmodel.HeroListViewModel
@@ -15,19 +16,28 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     // ViewModel 与宿主 Activity 绑定
-    val heroListViewModel: HeroListViewModel by viewModels()
-    val equipmentListViewModel: EquipmentListViewModel by viewModels()
-    val arcanaListViewModel: ArcanaListViewModel by viewModels()
+    private val heroListViewModel: HeroListViewModel by viewModels()
+    private val equipmentListViewModel: EquipmentListViewModel by viewModels()
+    private val arcanaListViewModel: ArcanaListViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.rootLayout)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.root_layout)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
             insets
         }
+
+        binding.heroListViewModel = heroListViewModel
+        binding.equipmentListViewModel = equipmentListViewModel
+        binding.arcanaListViewModel = arcanaListViewModel
     }
 }
